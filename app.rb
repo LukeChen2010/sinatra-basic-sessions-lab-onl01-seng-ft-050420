047164158344e4ba5c280aa52bb940ba2d557d0e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-29 19:52:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,9 +12,9 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    
-    @session << params.to_s
     @item = params[:item]
+    session[:item] = @item
+    @session = session
     
     return @item
   end
